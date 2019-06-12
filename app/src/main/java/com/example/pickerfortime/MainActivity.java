6ef681be7fb69e770c17cefcc8e2c981be356f21
ngyk-wm17@student.tarc.edu.my <Weiyi20780012>
2019-06-12 21:17:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pickerfortime/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.pickerfortime;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void processTimePickerResult(int hourOfDay, int minute) {
+        // Convert time elements into strings.
+        String hour_string = Integer.toString(hourOfDay);
+        String minute_string = Integer.toString(minute);
+        // Assign the concatenated strings to timeMessage.
+        String timeMessage = (hour_string + ":" + minute_string);
+
+        Toast.makeText(this, getString(R.string.time) + timeMessage,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void showTimePicker(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.timepicker));
+    }
+}
